529db69a1f9f03317575beac760871889c615c22
teopaulo <pablo.rosales14@gmail.com>
2022-06-07 16:27:08 -0700

    Saving a production cors, & a development cors

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const sess = {
 };
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+/* // Development-Cors
+   app.use(cors());
+ */
+// Production-Cors
 app.use(cors({
   origin:"https://movementmapper-front.herokuapp.com"
 }));
