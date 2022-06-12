a7c4513366a496504f94dd24e153ef779bd50eb8
teopaulo <pablo.rosales14@gmail.com>
2022-06-12 15:06:30 -0700

    changing restrictions on cors

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const sess = {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // Development-Cors
-  //  app.use(cors());
+   app.use(cors());
 
 // Production-Cors
-app.use(cors({
-  origin:"https://movementmapper-front.herokuapp.com"
-}));
+// app.use(cors({
+//   origin:"https://movementmapper-front.herokuapp.com"
+// }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
